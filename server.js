183c4255581eb6b0c66f1799155a9d0255849688
Umar Farooq <umar.farooq@docusign.com>
2019-06-10 10:53:01 -0500

    added body parser

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser'); 
+
 const port = process.env.PORT || 3000
 
+app.use(bodyParser());
+
 app.post('/', (req, res) => {
+	
 	res.send(req.body);
-	//console.log(req.body);
+	
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
