8d6b2bf023689e9b6521d9130b288710d87b1f17
Umar Farooq <umar.farooq@docusign.com>
2019-06-10 10:44:19 -0500

    sending response to postman

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.post('/', (req, res) => {
-	res.send('Hello World!');
-	console.log(req.body);
+	res.send(req.body);
+	//console.log(req.body);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
